64de32bde695ee4e7727afbf772c34279b2c9d7d
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-19 16:45:55 +0800

    Update config2.php

--- config2.php
+++ config2.php
@@ -42,7 +42,7 @@ if(isset($_GET['code'])){
     $permissions = ['email'];
     $facebook_login_url = $facebook_helper->getLoginUrl('https://apitest-ledesma.herokuapp.com/', $permissions);  
  
-    $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="images/facebook.png" /></a></div>';
+    $facebook_login_url = '<div align="center"><a href="'.$facebook_login_url.'"><img src="images/facebook.png" style="width: 20vw; height: 7vh;"/></a></div>';
 }
 
 ?>
