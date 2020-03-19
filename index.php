ab76387a2cb46ad90181134a85205941b560f331
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-19 18:50:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if($login_button == true){
       background-color: #3b5998;
   border: none;
   color: white;
-  padding: 15px 23.3px;
+  padding: 15px 23.1px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
