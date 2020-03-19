e5197985c7250f76c927f4d9701380d20443a431
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-19 17:13:29 +0800

    Update home.php

--- home.php
+++ home.php
@@ -6,10 +6,10 @@
   <img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:40%; margin: 0 auto;">
   <div style="text-align: center;
   padding: 10px 20px;">
-  <h1>Profile Picture</h1>
+  <h2><?php echo $_SESSION['user_email_address'] ?></h2>
   </div>
 </div>
-<h2>Welcome!: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h2>
-//Email: <?php echo $_SESSION['user_email_address'] ?>
-<br>
-<h3><a href="signout.php">Logout</a></h3>
+<h2>Welcome, <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?>! </h2>
+
+<br><br>
+<a href="signout.php"><div style="padding: 5px 5px 5px 5px; background-color: red;">Logout</div></a></h3>
