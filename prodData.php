501c74fd2be39c26d011fbf92d701cd2821e98dc
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-19 18:39:27 +0800

    Update prodData.php

--- prodData.php
+++ prodData.php
@@ -7,33 +7,50 @@ $details = array('records' => $data);
 $list = $details['records'];
 
 ?>
+<style>
+   .info-row{
+      margin: 0 auto; 
+      margin-bottom: 5vh;
+      width: 70%; 
+      display: flex; 
+      flex-direction: row; 
+      justify-content: space-between;
+   }
+</style>
+
    <div class="empty-view"></div>
 <div class="header-top"><h1>Product Profile</h1></div>
 
 <div style="width: 100%;display: flex; flex-direction: column;">
-   <div style="width: 70%; display: flex; flex-direction: row; text-align: left;">
+   <div class="info-row">
       <div><h2>Name:</h2></div>
       <div><h2><?php echo $list['name'];?></h2></div>
    </div>
-   <div>
-       <h2>Price:</h2> <?php echo "P".$list['price'];?>
+   <div class="info-row">
+      <div>
+          <h2>Price:</h2>
+      </div>
+       <div>
+          <h2> <?php echo "P".$list['price'];?></h2>
+      </div>
    </div>
-   <div class="empty-small"></div>
-   <div class="empty-small"></div>
-   <div class="empty-small"></div>
-   <div>
-       <h2>Description:</h2> <?php echo $list['description'];?>
+   <div class="info-row">
+      <div>
+          <h2>Description:</h2>
+      </div>
+       <div>
+          <h2> <?php echo $list['description'];?></h2>
+      </div>
    </div>
-   <div class="empty-small"></div>
-   <div class="empty-small"></div>
-   <div class="empty-small"></div>
-   <div>
-       <h2>Category:</h2> <?php echo $list['category_name'];?>
-   </div><br><br>
-   <div class="empty-view"></div>
-
-   <div class="empty-view"></div>
-<div>
+   <div class="info-row">
+      <div>
+          <h2>Category:</h2>
+      </div>
+       <div>
+          <h2><?php echo $list['category_name'];?>
+      </div>
+   </div>
+ </div>
 
 <div class="div-align-buttons">
    <a href="index.php?module=editProd&id=<?php echo $list['id'];?>""><div class="button-custom">EDIT</div></a><br>
