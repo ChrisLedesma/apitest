cc8591084e726e5a03ad3b7041cf3b33fafbb57b
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-03-19 16:06:18 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,9 @@
+<style>
+  .sticky-content{
+    displaY: hidden;
+  }
+</style>
+
 <div style="background-color: white;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   margin-bottom: 25px;
@@ -7,9 +13,9 @@
   <img src="<?php echo $_SESSION["user_image"] ?>" alt="profile pic" style="width:100%">
   <div style="text-align: center;
   padding: 10px 20px;">
-  <h1>Profile Picture</h1><!-- credits: john paul maja :D <3 -->
-  </div><!-- credits: john paul maja :D <3 -->
-</div><!--credits by m-a-j-a-->
+  <h1>Profile Picture</h1>
+  </div>
+</div>
 <h1>Name: <?php echo $_SESSION['user_first_name'] ?> <?php echo $_SESSION['user_last_name'] ?> </h1>
 <h1>Email: <?php echo $_SESSION['user_email_address'] ?></h1>
 
